cbef25f45445a7f2a4b339a91e23fb70a8657021
Cyberbells <info.cyberbells@gmail.com>
2025-10-24 16:15:32 +0530

    Delete Mobile directory

--- Mobile/src/contexts/ImagePath.js
+++
@@ -1,5 +0,0 @@
-export default {
-    backgroundImage: require('../assets/image/Background.png'),
-    googleImage: require('../assets/image/google.png')
-
-};
--- Mobile/src/styles/GolbalFonts.js
+++
@@ -1,9 +0,0 @@
-const Fonts = {
-  RubikBold: 'Rubik-Bold',
-  RubikMedium: 'Rubik-Medium',
-  RubikRegular: 'Rubik-Regular',
-  RubikSemiBold: 'Rubik-SemiBold',
-  RubikExtraBold:'Rubik-ExtraBold'
-};
-
-export default Fonts;
